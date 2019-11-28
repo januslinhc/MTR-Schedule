f33ae3462c1f6293f7420cf5918b3ac9906df5bf
Janus Lin <januslin@dbs.com>
2019-11-28 17:14:48 +0800

    first commit

---
+++ precache-manifest.46c9ab61649528ccb6d73a6713a75bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a96e52e369fed63d5f7f3e12ef37ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4186dd8f2e6bb56bde22",
+    "url": "/static/css/main.f56e5c7f.chunk.css"
+  },
+  {
+    "revision": "02de518c5b5fc45b6fef",
+    "url": "/static/js/2.c601bfa6.chunk.js"
+  },
+  {
+    "revision": "4186dd8f2e6bb56bde22",
+    "url": "/static/js/main.569fbec1.chunk.js"
+  },
+  {
+    "revision": "cf1962f39c291dce98bf",
+    "url": "/static/js/runtime-main.801a17ee.js"
+  }
+]);
